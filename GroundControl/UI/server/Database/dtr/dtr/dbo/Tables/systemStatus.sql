f90b394b02532604e06baa6688e02fb3b5f9555a
Timothy Woodbridge <timothy.woodbridge@navy.mil>
2022-05-23 07:57:58 -0400

    adding database files for basic system status table

---
+++ GroundControl/UI/server/Database/dtr/dtr/dbo/Tables/systemStatus.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[systemStatus] (
+    [ID]                 INT           IDENTITY (1, 1) NOT NULL,
+    [blimpLastHeartbeat] DATETIME      NULL,
+    [cameraDetectionStr] VARCHAR (MAX) NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+

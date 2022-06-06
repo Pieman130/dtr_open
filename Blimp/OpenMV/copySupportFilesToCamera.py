c808845df99ecfc3239f6376a67c0aab6340cacd
barkerap <barker@umail.iu.edu>
2022-06-06 13:37:39 -0400

    added support for MacOS

--- Blimp/OpenMV/copySupportFilesToCamera.py
+++ Blimp/OpenMV/copySupportFilesToCamera.py
@@ -1,9 +1,21 @@
+#Works for Windows and MacOS, will not work for Linux
 import os
 from distutils.dir_util import copy_tree
-currDir = os.path.dirname(__file__);
-# copy subdirectory example
-from_directory = currDir + "\support"
-to_directory = "E:/"
+if os.name == 'posix': #Unix-like machines
+    currDir = os.path.dirname(os.path.realpath(__file__))
+
+    if os.uname().sysname == 'Darwin': #Mac machines
+       to_directory = "/Volumes/NO NAME"
+       from_directory = currDir +"/support"
+    else:
+        pass #TODO fix for Linux machines
+
+else: #Windows machines
+    currDir = os.path.dirname(__file__);
+    to_directory = "E:/"
+    from_directory = currDir + "\support"
 
 copy_tree(from_directory, to_directory)
+
+
 

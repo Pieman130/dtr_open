6c8d5144702d572193c67a088f6cf4269d6ada08
Timothy Woodbridge <timothy.woodbridge@navy.mil>
2022-08-04 16:06:25 -0400

    option1 chosen architecture - initial software commit

---
+++ Blimp/OpenMV/support/externalSensors.py
@@ -0,0 +1,48 @@
+import sensor
+
+from machine import Pin
+
+
+# globals
+class Sensors:
+    def __init__():
+        self.irSensor: None
+        self.camera: None
+
+class RawData:
+    def __init__():
+        self.img = None
+        self.irSensor = None
+
+rawData = RawData
+sensors = Sensors
+
+def initialize(): # https://github.com/mavlink/c_library_v1/blob/master/checksum.h
+    global sensors
+    output = 0
+    print("initialize sensors")
+    initializeCamera()
+    sensors.irSensor = Pin('P4',Pin.IN,Pin.PULL_NONE)
+    return output
+    
+
+def initializeCamera():
+    global sensors
+    print("initializing camera")
+
+    sensor.reset()
+    sensor.set_pixformat(sensor.RGB565)
+    sensor.set_framesize(sensor.QVGA)
+    sensor.skip_frames(time = 2000)
+    sensors.camera = sensor
+
+
+def collectData():
+    global sensors
+    global rawData
+    output = 0
+    print("collecting data")
+    rawData.img = sensors.camera.snapshot()
+    rawData.irSensor = sensors.irSensor.value()
+
+    return output

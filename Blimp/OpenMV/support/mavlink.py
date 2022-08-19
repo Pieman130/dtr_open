7f642bbd3a870e7d56700f311322b946e44782ac
Timothy Woodbridge <timothy.woodbridge@navy.mil>
2022-08-19 14:21:21 -0400

    reading lidar data successfully

--- Blimp/OpenMV/support/mavlink.py
+++ Blimp/OpenMV/support/mavlink.py
@@ -168,6 +168,40 @@ class MavLink():
             self.parse_mavlink(result)
 
 
+    def parse_distance(self,ser_msg):
+        '''
+        0th B = preamble (254)
+        1st B = payload length
+        2nd B = seq #
+        3rd B = sys id
+        4th B = Comp id
+        5th B = Msg id (message type)
+        6th - n B = payload
+        n-2 = 1st byte chksum
+        n-1 = 2nd byte chksum
+        '''
+        header = []
+        try:
+            for i in range(6):
+                header.append(ser_msg[i])
+            if ser_msg[0] != 254: #if inital byte not 254, it is misaligned message, ignore
+                print('misaligned')
+                return None
+            else:
+                if ser_msg[5] == 132:
+                    payload = ser_msg[6:-2]
+                    dist = int.from_bytes(payload[8:10], "little")
+                    print("Range: ", dist)
+                    return (dist)
+                else:
+                    
+                    print('other else')
+                    print(ser_msg[5])
+                    return None
+        except IndexError:
+            return None
+
+
     def parse_mavlink(self,ser_msg):
         '''
         0th B = preamble (254)

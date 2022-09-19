8ed7a0d8a90064cda8beacea08dcabd69e3f462e
Owen Vogelgesang <ovogelge@googlemail.com>
2022-09-19 11:50:23 -0400

    Finished ParseRCSwitchPositions

--- Blimp/OpenMV/support/processing.py
+++ Blimp/OpenMV/support/processing.py
@@ -12,45 +12,48 @@ else:
 SERVO_OPEN = 0
 SERVO_CLOSED = 45
 
-def parseSensorData(): # https://github.com/mavlink/c_library_v1/blob/master/checksum.h    
+
+def parseSensorData():  # https://github.com/mavlink/c_library_v1/blob/master/checksum.h
     output = 0
     print("parsing sensor data")
 
     parseIrSensorData()
-    dataClasses.data.colorDetected = imageProcessing.colorDetectedByCamera(dataClasses.rawData.img)
+    dataClasses.data.colorDetected = imageProcessing.colorDetectedByCamera(
+        dataClasses.rawData.img)
 
     print("color detected: " + dataClasses.data.colorDetected)
 
-    dataClasses.data.aprilTagFound = imageProcessing.lookForAprilTag(dataClasses.rawData.img)        
+    dataClasses.data.aprilTagFound = imageProcessing.lookForAprilTag(
+        dataClasses.rawData.img)
     dataClasses.data.isAprilTagDetected = dataClasses.data.aprilTagFound.foundIt
 
     parseLidarData()
 
-
-    #overwrite for testing purposes the processed info, to test state machine 
+    # overwrite for testing purposes the processed info, to test state machine
     # for running on pc.
-    if(dataClasses.config.isMicroPython == False): 
+    if (dataClasses.config.isMicroPython == False):
         dataClasses.data.colorDetected = dataClasses.gndStationCmd.mockSensor_greenDetected
         dataClasses.data.isAprilTagDetected = dataClasses.gndStationCmd.mockSensor_aprilTagDetected
 
-
     return output
 
-def parseIrSensorData():    
-    dataClasses.data.irData = not bool(dataClasses.rawData.irSensor)    
-    
+
+def parseIrSensorData():
+    dataClasses.data.irData = not bool(dataClasses.rawData.irSensor)
+
     print('ir sensor: ' + str(dataClasses.data.irData))
-   
+
 
 def parseLidarData():
-    if(dataClasses.rawData.lidar_cm != None):
-        rawDist_ft = dataClasses.rawData.lidar_cm /30.48
-        correctedDist_ft = attitudeCorrectDistance(rawDist_ft, dataClasses.rawData.imu_roll, dataClasses.rawData.imu_pitch)
+    if (dataClasses.rawData.lidar_cm != None):
+        rawDist_ft = dataClasses.rawData.lidar_cm / 30.48
+        correctedDist_ft = attitudeCorrectDistance(
+            rawDist_ft, dataClasses.rawData.imu_roll, dataClasses.rawData.imu_pitch)
         dataClasses.data.lidarDistance_ft = correctedDist_ft
         print('lidar distance (ft): ' + str(correctedDist_ft))
 
 
-def attitudeCorrectDistance(measDist=0.0,roll_rad=0.0,pitch_rad=0.0):
+def attitudeCorrectDistance(measDist=0.0, roll_rad=0.0, pitch_rad=0.0):
     # correctedDist = math.cos(math.sqrt(roll_rad**2 + pitch_rad**2)) * measDist # Not sure which of these methods will be faster on the uC
     correctedDist = math.cos(roll_rad) * math.cos(pitch_rad) * measDist
     return correctedDist
@@ -68,24 +71,42 @@ def parseDoorPosition():
 def parseRCSwitchPositions():
     currentValue = dataClasses.rawData.rc_sw_door
 
-    # Iterate over states, setting state appropriately 
-    currentDelta = 500 / (len(dataClasses.DoorControlState()) - 1) # Split up range evenly
+    # Iterate over states, setting state appropriately
+    # Split up range evenly
+    currentDelta = 500 / (len(dataClasses.DoorControlState()) - 1)
     for state in dataClasses.DoorControlState():
         if state[0] + currentDelta >= currentValue and state[0] - currentDelta <= currentValue:
             dataClasses.ProcessedData.sw_door_control = state[1]
             break
-    else:
+    else:   # I hate for/else loops but this is a good way of handling an unexpected error
         dataClasses.ProcessedData.sw_door_control = None
 
+    currentValue = dataClasses.rawData.rc_sw_flt_mode
+    currentDelta = 500 / (len(dataClasses.FlightModeState()) - 1)
+    for state in dataClasses.FlightModeState():
+        if state[0] + currentDelta >= currentValue and state[0] - currentDelta <= currentValue:
+            dataClasses.ProcessedData.sw_flight_mode = state[1]
+            break
+    else:
+        dataClasses.ProcessedData.sw_flight_mode = None
 
-#def distanceToBall():    
+    # There's no switch for this in ProcessedData.
+    # currentValue = dataClasses.rawData.rc_sw_st_cntl
+    # currentDelta = 500 / (len(dataClasses.AutonomousModeState()) - 1)
+    # for state in dataClasses.AutonomousModeState():
+    #     if state[0] + currentDelta >= currentValue and state[0] - currentDelta <= currentValue:
+    #         pass
+    #         break
+    # else:
+    #     pass
 
-#def distanceToCeiling():
-    
+
+# def distanceToBall():
+
+# def distanceToCeiling():
 
 
-#def distanceToFloor(): #may not actually ever do this one.
- 
+# def distanceToFloor(): #may not actually ever do this one.
 
-#def distanceToGoal():
- 
+
+# def distanceToGoal():

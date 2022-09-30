c022f6ef20ec32c5012c0f13869c391a078830d6
Owen Vogelgesang <ovogelge@googlemail.com>
2022-09-30 15:32:17 -0400

    Began implementing control switches

--- Blimp/OpenMV/support/flightActions.py
+++ Blimp/OpenMV/support/flightActions.py
@@ -4,23 +4,25 @@ import mavlink
 import logger
 from pid import Controller
 
+
 class ExitCriteria:
     def __init__(self):
         self.list = []
-       
-    def add(self,variableName,value):
-        criterion = ExitCriterion(variableName,value)
+
+    def add(self, variableName, value):
+        criterion = ExitCriterion(variableName, value)
         self.list.append(criterion)
 
+
 class ExitCriterion:
-    def __init__(self,variableName,value):
+    def __init__(self, variableName, value):
         self.variableName = variableName
         self.value = value
 
 
 class FlightAction:
-    def __init__(self,description,controls,exitCriteria,comms,hw):
-        
+    def __init__(self, description, controls, exitCriteria, comms, hw):
+
         self.description = description
         self.timeClock = 0
         self.startTime = None
@@ -34,33 +36,51 @@ class FlightAction:
         self.pid_up = Controller()
         self.pid_throttle = Controller()
 
-
     def reset(self):
         self.startTime = None
         self.timeClock = 0
-
 
     def updateTime(self):
         if self.startTime == None:
             self.startTime = time.time()
         else:
             self.timeClock = time.time() - self.startTime
-        
 
     def execute(self):
         self.updateTime()
         logger.log.verbose(self.description + " -  Maneuver")
-        logger.log.verbose("\ttime: " + str(self.timeClock))                
-        self.mavlink.setControls(self.controls)
-        
-        if self.controls.servo == 1:
+        logger.log.verbose("\ttime: " + str(self.timeClock))
+
+        if dataClasses.data.sw_flight_mode == "manual":
+            # Is there a way to dynamically program the mavlink to do this on its own?
+            # TODO: Implement raw passthrough
+            self.mavlink.setControls(self.controls)4
+
+        elif dataClasses.data.sw_flight_mode == "assisted":
+            # Altitude hold and yaw stability ?
+            # TODO: Implement actual altitude hold and yaw controls
+            self.mavlink.setControls(self.controls)
+
+        else:
+            # This is the autonomous section - where the code controls the blimp
+            # What should go here? I'm just putting in this code as a boilerplate.
+            # TODO: Implement actual autonomous controls.
+            self.mavlink.setControls(self.controls)
+
+        if dataClasses.data.sw_door_control == "closed":
+            self.hw.closeDoor()
+        elif dataClasses.data.sw_door_control == "open":
             self.hw.openDoor()
         else:
-            self.hw.closeDoor()
+            # TODO: Implement proper autonomous door control
+            if self.controls.servo == 1:
+                self.hw.openDoor()
+            else:
+                self.hw.closeDoor()
 
-        logger.log.info("Controls Values: {}".format(self.controls.printValues()))
-        
-    
+        logger.log.info("Controls Values: {}".format(
+            self.controls.printValues()))
+
     def sendNoop(self):
         blankControls = Controls()
         self.mavlink.setControls(blankControls)
@@ -71,62 +91,63 @@ class FlightAction:
         else:
             return False
 
-    def isSensorExitCriteriaMet(self):    
+    def isSensorExitCriteriaMet(self):
         numCriterionUnmet = 0
         numCriterionMet = 0
         for item in self.exitCriteria.list:
             if not self.isCriteriaTimeClock(item.variableName):
                 if item.value == self.getProperty(item.variableName):
-                    numCriterionMet = numCriterionMet + 1           
+                    numCriterionMet = numCriterionMet + 1
                 else:
-                    logger.log.verbose("NO MATCH (" + item.variableName + ") = " + str(item.value) + " != " + str(self.getProperty(item.variableName)) )                    
-                    numCriterionUnmet = numCriterionUnmet + 1               
+                    logger.log.verbose("NO MATCH (" + item.variableName + ") = " + str(
+                        item.value) + " != " + str(self.getProperty(item.variableName)))
+                    numCriterionUnmet = numCriterionUnmet + 1
 
         if numCriterionUnmet == 0 and numCriterionMet > 0:
             return True
         else:
-            return False        
-    
+            return False
+
     def isManeuverTimeoutReached(self):
         returnVal = False
         for item in self.exitCriteria.list:
             if self.isCriteriaTimeClock(item.variableName):
                 if self.timeClock > item.value:
                     returnVal = True
-        
+
         return returnVal
 
-
-    def isCriteriaTimeClock(self,value):
+    def isCriteriaTimeClock(self, value):
         if value == "timeClock":
             return True
         else:
             return False
 
-    def getProperty(self,name):
+    def getProperty(self, name):
         if name == "timeClock":
-            return getattr(self,name)
+            return getattr(self, name)
         else:
-            return getattr(self.data,name)
-
+            return getattr(self.data, name)
 
     def execute_assisted_altitude(self, height):
         '''take in desired distance to ceiling (height)
         maintain a pid controlled hover about that distance'''
         if height != None:
-            self.controls.up = self.pid_up.get_pid(height-self.data.lidarDistance) 
-            logger.log.info("Executing Assisted Altitude.  PID Up Value: {}".format(self.controls.up) )
+            self.controls.up = self.pid_up.get_pid(
+                height-self.data.lidarDistance)
+            logger.log.info(
+                "Executing Assisted Altitude.  PID Up Value: {}".format(self.controls.up))
 
 
 class Controls:
-    def __init__(self):                
-        self.yaw = 0 #-1 to 1
-        self.up = 0 #-1 to 1
-        self.throttle = 0 #-1 to 1
-        self.servo = 0 # 0 for OFF. 1 for ON.
-    def printValues(self):            
+    def __init__(self):
+        self.yaw = 0  # -1 to 1
+        self.up = 0  # -1 to 1
+        self.throttle = 0  # -1 to 1
+        self.servo = 0  # 0 for OFF. 1 for ON.
+
+    def printValues(self):
         logger.log.verbose("\t\tyaw: " + str(self.yaw))
         logger.log.verbose("\t\tup: " + str(self.up))
         logger.log.verbose("\t\tthrottle: " + str(self.throttle))
         logger.log.verbose("\t\tservo: " + str(self.servo))
-

2f4f56a2dbfe18daa211acb7d1c1093508547a0d
Caedjoe <80704743+Caedjoe@users.noreply.github.com>
2022-09-30 16:24:55 -0400

    ImageProcessing.py Correction
    Uploaded correct version of image processing file to find goals and balls. Created a branch to stop conflicts

--- Blimp/OpenMV/support/imageProcessing.py
+++ Blimp/OpenMV/support/imageProcessing.py
@@ -8,7 +8,6 @@ if dataClasses.config.isMicroPython:
 
 import time
 import math
-
 class EMA:
     def __init__(self, value, alpha = 0.5):
         self.value = value
@@ -21,50 +20,51 @@ class EMA:
 
 # Color Tracking Thresholds (L Min, L Max, A Min, A Max, B Min, B Max)
 # The below thresholds track in general red/green/blue things. You may wish to tune them...
-color_thresholds = [(30, 100, 15, 127, 15, 127), # ball
-              (30, 100, -64, -8, -32, 32), # yellow goal
-              (0, 30, 0, 64, -128, 0)]  # orange goal
+color_thresholds = [(30, 100, 15, 127, 15, 127), (30, 100, -64, -8, -32, 32), (0, 30, 0, 64, -128, 0)]
 pixels_threshold=50
 area_threshold=50
 margin = 10
 x_ema = None
 y_ema = None
+width = 0
 def colorDetectedByCamera(img):
-    width = 0
+    blobs = 0
     #search for ball in img
     biggestball = [0,0,0,0] #[x, y, width, height]
-    closest = [0,0,0,0]
-    for blob in img.find_blobs([color_thresholds[0]], pixels_threshold, area_threshold, merge=True, margin = margin):
+    currentball = [0,0,0,0] #[x, y, width, height]
+    for blobs in img.find_blobs([color_thresholds[0]], pixels_threshold = pixels_threshold, area_threshold = area_threshold, merge=True, margin = margin):
         currentball = [blob.cx(), blob.cy(), blob.rect()[2], blob.rect()[3]]
         if currentball[2] > biggestball[2]:
             biggestball = currentball
-    if blob != 0:
+    if blobs != 0:
         if x_ema == None:
-            x_ema = EMA(current[0], 1 - 0.7)
+            x_ema = EMA(currentball[0], 1 - 0.7)
         else:
-            x_ema.update(current[0])
+            x_ema.update(currentball[0])
         if y_ema == None:
-            y_ema = EMA(current[1], 1 - 0.7)
+            y_ema = EMA(currentball[1], 1 - 0.7)
         else:
-            y_ema.update(current[1])
+            y_ema.update(currentball[1])
         dataClasses.ProcessedData.ballx = x_ema
         dataClasses.ProcessedData.bally = y_ema
         width = biggestball[2]
+    #NEED TO DISCUSS HOW TO PICK GOAL
     if dataClasses.ProcessedData.goalColorChoice == "yellow":
         threshold = 1
-    elif dataClasses.ProcessedData.goalColorChoice == "orange":
+    else:
         threshold = 2
     #search for yellow goal
-    for blob in img.find_blobs([color_thresholds[threshold]], pixels_threshold, area_threshold, merge=True, margin = margin):
-        current = [blob.cx(), blob.cy(), blob.rect()[2], blob.rect()[3]]
-        if abs(160 - (current[0])) < abs(160 - (closest[0])): #instead of current[0], use pythagorean theorem to find closest ball
-            closest = current
+    for blob in img.find_blobs([color_thresholds[threshold]], pixels_threshold = pixels_threshold, area_threshold = area_threshold, merge=True, margin = margin):
+        currentgoal = [blob.cx(), blob.cy(), blob.rect()[2], blob.rect()[3]]
+        if abs(160 - (currentgoal[0])) < abs(160 - (closestgoal[0])): #instead of current[0], use pythagorean theorem to find closest ball
+            closestgoal = currentgoal
     if blob != 0:
         dataClasses.ProcessedData.goalColorDetected = True
-        dataClasses.ProcessedData.goalx = closest[0]
-        dataClasses.ProcessedData.goaly = closest[1]
-        dataClasses.ProcessedData.goalskew = closest[3]/closest[4]
+        dataClasses.ProcessedData.goalx = closestgoal[0]
+        dataClasses.ProcessedData.goaly = closestgoal[1]
+        dataClasses.ProcessedData.goalskew = closestgoal[3]/closestgoal[4]
     return
+
 
 class TagInfo:
     def __init__(self):

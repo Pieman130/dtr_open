ced7d1b133ba754649bd14e3b5c4b0dc5bb64245
Caedjoe <80704743+Caedjoe@users.noreply.github.com>
2022-11-04 13:51:33 -0400

    Update imageprocessing.py with new functions
    Began Integrating functions from IDE prototype, from OpenMV_ide_prototype.py to imageProcessing.py to run on final system

--- Blimp/OpenMV/support/imageProcessing.py
+++ Blimp/OpenMV/support/imageProcessing.py
@@ -18,55 +18,186 @@ class EMA:
     def get_value(self):
         return self.value
 
-# Color Tracking Thresholds (L Min, L Max, A Min, A Max, B Min, B Max)
-# The below thresholds track in general red/green/blue things. You may wish to tune them...
-color_thresholds = [(30, 100, 15, 127, 15, 127), (30, 100, -64, -8, -32, 32), (0, 30, 0, 64, -128, 0)]
-pixels_threshold=50
-area_threshold=50
-margin = 10
+#Copied from OpenMV_ide_prototype.py, edited to better fit code structure
+import sensor, image, time, math, ustruct, sys, pyb, pid
+from pyb import USB_VCP, Pin, Timer, Servo
+
+ledRed = pyb.LED(1) # Initiates the red led
+ledGreen = pyb.LED(2) # Initiates the green led
+ledBlue = pyb.LED(3) # Initiates the blue led
+ledIR = pyb.LED(4) # Initiates the IR led
+
+
+thresholds = [(60, 82, -48, -11, -128, 127), #yellow 0
+              (0, 70, -59, -12, -10, 54), #green 1
+              (17, 100, 14, 70, 34, 127)] #orange 2
+
+
+#These are the sensor parameters for color detection to work optimally:
+"""
+sensor.reset()
+sensor.set_pixformat(sensor.RGB565)
+sensor.set_framesize(sensor.QVGA)
+sensor.skip_frames(time = 200)
+sensor.set_auto_gain(False) # must be turned off for color tracking
+sensor.set_auto_whitebal(True) # must be turned off for color tracking (I ignored this and kept it on)
+clock = time.clock()
+"""
+# data on selected goal (goalx and goaly are instances of EMA)
+goalx_ema = None
+goaly_ema = None
+SQerror = 0
+
+# data on selected ball (all three are instances of EMA)
 x_ema = None
 y_ema = None
-width = 0
-def colorDetectedByCamera(img):
-    blobs = 0
-    #search for ball in img
-    biggestball = [0,0,0,0] #[x, y, width, height]
-    currentball = [0,0,0,0] #[x, y, width, height]
-    currentgoal = [0,0,0,0] #[x, y, width, height]
-    closestgoal = [0,0,0,0] #[x, y, width, height]
-    for blobs in img.find_blobs([color_thresholds[0]], pixels_threshold = pixels_threshold, area_threshold = area_threshold, merge=True, margin = margin):
-        currentball = [blob.cx(), blob.cy(), blob.rect()[2], blob.rect()[3]]
-        if currentball[2] > biggestball[2]:
-            biggestball = currentball
-    if blobs != 0:
-        if x_ema == None:
-            x_ema = EMA(currentball[0], 1 - 0.7)
-        else:
-            x_ema.update(currentball[0])
-        if y_ema == None:
-            y_ema = EMA(currentball[1], 1 - 0.7)
-        else:
-            y_ema.update(currentball[1])
-        dataClasses.ProcessedData.ballx = x_ema
-        dataClasses.ProcessedData.bally = y_ema
-        width = biggestball[2]
-        degree = width/ .225
-    #NEED TO DISCUSS HOW TO PICK GOAL
-    if dataClasses.ProcessedData.goalColorChoice == "yellow":
-        threshold = 1
+rect_ema = None #This is bounding box of ball, used to determine what is the closest
+
+#misc. variables
+alpha_rect = .85 # alpha value of the rectangle that bounds the ball
+goal_alpha = .85 # alpha value of the rectangle that bounds the goal
+ball_alpha = .85 # alpha value of the centroid of the ball
+
+goal_xerror = 0 # Distance in pixels away from the goal horizontally from center (int)
+goal_yerror = 0 # Distance in pixels away from the goal vertically from center (int)
+
+ball_xerror = 0 # Distance in pixels away from the ball horizontally (int)
+ball_yerror = 0 # Distance in pixels away from the ball vertically (int)
+
+dist_ball = 0 # (float)
+dist_goal = 0 # (float)
+
+def find_ball(img):
+    blobs = img.find_blobs([thresholds[1]], pixels_threshold=50, area_threshold=50, merge=True,  margin = 10)
+    biggest = [0,0,0,0] #[x, y, width, height]
+    global x_ema
+    global y_ema
+    global ball_xerror
+    global ball_yerror
+    global rect_ema
+    global alpha_rect
+    global dist_ball
+    for blob in blobs:
+        current = [blob.cx(), blob.cy(), blob.rect()[2], blob.rect()[3]]
+        if current[2] > biggest[2]:
+            biggest = current
+            r = blob.rect()
+            if x_ema == None:
+                x_ema = EMA(current[0], ball_alpha)
+                rect_ema = [EMA(r[0], alpha_rect), EMA(r[1], alpha_rect), EMA(r[2], alpha_rect), EMA(r[3], alpha_rect)]
+                y_ema = EMA(current[1], ball_alpha)
+            else:
+                x_ema.update(current[0])
+                y_ema.update(current[1])
+                rect_ema[0].update(r[0])
+                rect_ema[1].update(r[1])
+                rect_ema[2].update(r[2])
+                rect_ema[3].update(r[3])
+            if (rect_ema[2].get_value() != 0):
+                dist_ball = 22/(math.tan((rect_ema[2].get_value() * .22125)/2)) #rect_ema[2].getvalue() gives ball width, use equation for pixel width to meters away
+            #img.draw_rectangle(blob.rect())
+            img.draw_rectangle([round(ema.get_value()) for ema in rect_ema], 2)
+            img.draw_string(round(rect_ema[0].get_value()),round(rect_ema[1].get_value()), " Ball", [0, 0, 255], mono_space = False)
+            #img.draw_cross(blob.cx(), blob.cy())
+            img.draw_cross(round(x_ema.get_value()), round(y_ema.get_value()), 2)
+            ball_xerror = round(x_ema.get_value()) - 160
+            ball_yerror = round(y_ema.get_value()) - 120
+
+def find_yellow_goal(img):
+    blobs = img.find_blobs([thresholds[0]], pixels_threshold=3, area_threshold=12, merge=True, margin=10)
+    biggest = [160,120,0,0] #[cx, cy, width, height]
+    counter = 0
+    global dist_goal
+    global goalx_ema
+    global goaly_ema
+    global SQerror
+    global goal_xerror
+    global goal_yerror
+    for blob in blobs:
+        current = [blob.cx(), blob.cy(), blob.rect()[2], blob.rect()[3]]
+        img.draw_rectangle(blob.rect())
+        #img.draw_cross(current[0], current[1])
+        counter = counter + 1
+        width = current[2]
+        height = current[3]
+        SQerror = width/height
+        if current[3] > biggest[3]:
+            biggest = current
+    img.draw_string(biggest[0], biggest[1],"Selected Yellow Goal",[0, 0, 255] , mono_space = False)
+    img.draw_cross(biggest[0], biggest[1])
+    if goalx_ema == None:
+        goalx_ema = EMA(biggest[0], goal_alpha)
+        goaly_ema = EMA(biggest[1], goal_alpha)
     else:
-        threshold = 2
-    #search for yellow goal
-    for blob in img.find_blobs([color_thresholds[threshold]], pixels_threshold = pixels_threshold, area_threshold = area_threshold, merge=True, margin = margin):
-        currentgoal = [blob.cx(), blob.cy(), blob.rect()[2], blob.rect()[3]]
-        if abs(160 - (currentgoal[0])) < abs(160 - (closestgoal[0])): #instead of current[0], use pythagorean theorem to find closest ball
-            closestgoal = currentgoal
-    if blob != 0:
-        dataClasses.ProcessedData.goalColorDetected = True
-        dataClasses.ProcessedData.goalx = closestgoal[0]
-        dataClasses.ProcessedData.goaly = closestgoal[1]
-        dataClasses.ProcessedData.goalskew = closestgoal[3]/closestgoal[4]
-    return
+        goalx_ema.update(biggest[0])
+        goaly_ema.update(biggest[1])
+    if (biggest[3] != 0):
+        dist_goal = 42 / math.tan((biggest[3] * .23166/2)) # change parameters to determine distance from goal with known values, biggest[3] is height of goal
+    img.draw_cross(round(goalx_ema.get_value()), round(goaly_ema.get_value()), color=[0,0,0])
+    goal_xerror = 160 - round(goalx_ema.get_value())
+    goal_yerror = round(goaly_ema.get_value()) - 120
+
+def find_orange_goal(img):
+    blobs = img.find_blobs([thresholds[2]], pixels_threshold=3, area_threshold=12, merge=True, margin=10)
+    biggest = [160,120,0,0] #[cx, cy, width, height]
+    counter = 0
+    global dist_goal
+    global goalx_ema
+    global goaly_ema
+    global SQerror
+    global goal_xerror
+    global goal_yerror
+    for blob in blobs:
+        current = [blob.cx(), blob.cy(), blob.rect()[2], blob.rect()[3]]
+        img.draw_rectangle(blob.rect())
+        #img.draw_cross(current[0], current[1])
+        counter = counter + 1
+        width = current[2]
+        height = current[3]
+        SQerror = width/height
+        if current[3] > biggest[3]:
+            biggest = current
+    img.draw_string(biggest[0], biggest[1],"Selected Orange Goal",[0, 0, 255] , mono_space = False)
+    img.draw_cross(biggest[0], biggest[1])
+    if goalx_ema == None:
+        goalx_ema = EMA(biggest[0], goal_alpha)
+        goaly_ema = EMA(biggest[1], goal_alpha)
+    else:
+        goalx_ema.update(biggest[0])
+        goaly_ema.update(biggest[1])
+    if (biggest[3] != 0):
+        dist_goal = 42 / math.tan((biggest[3] * .23166/2)) # change parameters to determine distance from goal with known values, biggest[3] is height of goal
+    img.draw_cross(round(goalx_ema.get_value()), round(goaly_ema.get_value()), color=[0,0,0])
+    goal_xerror = 160 - round(goalx_ema.get_value())
+    goal_yerror = round(goaly_ema.get_value()) - 120
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 class TagInfo:
